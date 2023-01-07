921241203953823aed8df2b209cf36bab10041ea
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-07 13:01:39 +0800

    实现代理接口

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import  requests
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/agent/")
+async def agent(url):
+    #访问url，获取返回值，并返回
+    resp = requests.get(url)
+    return resp.text
+
+# 运行指令：
+'''
+uvicorn main:app --reload
+'''
