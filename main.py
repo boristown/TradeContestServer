9e671a962d247c71a001938fea9ff9d1492a90d5
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-07 19:08:05 +0800

    B接口

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    resp = requests.get("https://fapi.binance.com/fapi/v1/ticker/price")
+    resp = requests.get("https://wwww.binance.com/fapi/v1/ticker/price")
     return resp.text
 
 @app.get("/agent/")
-def agent(url):
+async def agent(url):
     #访问url，获取返回值，并返回
     print("req:",url)
     resp = requests.get(url)
     print("res:",resp.text)
+    return resp.text
+
+@app.get("/B/")
+async def B(x):
+    url = "https://wwww.binance.com/" + x
+    resp = requests.get(url)
     return resp.text
 
 # 运行指令：
