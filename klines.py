4bd3614ce6207fdbce9e4988c464a15aaa0d4a73
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-18 13:45:36 +0800

    Update klines.py

--- klines.py
+++ klines.py
@@ -94,7 +94,7 @@ def draw_klines(symbol, interval='1h', start_time=None, end_time=None, indicator
         datazoom_opts = [opts.DataZoomOpts(pos_bottom="0%")], 
         title_opts = title_opts
         )
-    klinegrid=Grid(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
+    klinegrid=Grid(init_opts=opts.InitOpts(theme=ThemeType.LIGHT,width="350px", height="550px",))
     klinegrid.add(kline,grid_opts=opts.GridOpts(pos_left='25%'))
     klinegrid.render(filename_html)
     return filename_html
