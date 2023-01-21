328b39614730b39a15057ef96fc1240b28996d39
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-21 08:44:41 +0800

    设计初始画面

--- pywebio_app.py
+++ pywebio_app.py
@@ -1,5 +1,13 @@
 from pywebio import *
 
+#实现初始画面：
+#首先显示币种名称
+#然后显示k线图
+#之后是设置列（交易货币[USDT/BTC]，K线周期，时间窗口）
+#底部是可以排序和滑动的市场清单（列：市场名，价格，成交额，涨幅）
+#当选择列表中的市场名的时候，切换顶部的市场
+#默认排序：按成交额降序排列
 def pywebio_run():
     name = input.input("what's your name")
     output.put_text("hello", name)
+
