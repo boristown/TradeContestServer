f4ad76736d40a2b00ca1090baa365fc008016326
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-22 14:01:44 +0800

    ui upd

--- pywebio_app.py
+++ pywebio_app.py
@@ -16,10 +16,6 @@ down_triangle = '▼'
 #当选择列表中的市场名的时候，切换顶部的市场
 #默认排序：按成交额降序排列
 def pywebio_run():
-    def change_symbol(symbol):
-        pass
-        #set_scope('klines', clear=True)
-        #put_klines(symbol)
     interval = '1d'
     symbol = 'BTCUSDT'
     current_time = int(time.time() * 1000)
@@ -28,7 +24,16 @@ def pywebio_run():
     put_row([
         put_select('selectBase', options=['USDT', 'BTC']),
         #默认值是最近1天
-        put_select('selectPeriod', options=['最近1天','最近1小时','最近2小时','最近6小时','最近12小时','最近3天','最近7天','最近1月','最近3月','最近6月','最近1年'])
+        put_select('selectPeriod', 
+        options=[
+            '最近1小时','最近2小时','最近6小时',
+            '最近12小时','最近1天','最近3天',
+            '最近7天','最近1月','最近3月',
+            '最近6月','最近1年'],
+            value='最近1天',
+            #onchange=None,
+            #init_run=False
+            )
     ])
     #put_table([['市场名', '价格', '成交额', '涨幅']])
     put_text(symbol)
@@ -36,7 +41,15 @@ def pywebio_run():
     put_html(html)
     #put_select('交易货币', options=['USDT', 'BTC'])
     put_row([
-        put_select('selectInterval', options=['1小时','3分钟','5分钟','15分钟','30分钟','2小时','6小时','1天','3天','7天','14天'])
+        put_select('selectInterval', 
+        options=[
+            '3分钟','5分钟','15分钟',
+            '30分钟','1小时','2小时',
+            '6小时','1天','3天',
+            '7天','14天'
+            ],
+        value='1小时'
+            )
     ])
     put_buttons(['市场名', '价格', '成交额'+down_triangle, '涨幅'], onclick=change_symbol)
     #put_input('input', label='This is a input widget')
