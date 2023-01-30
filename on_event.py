deb4d07b444e57878c530786a961d6dca128753a
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-01-30 12:34:04 +0800

    修复空值bug

--- on_event.py
+++ on_event.py
@@ -169,6 +169,83 @@ def set_sort(cli,label):
         cli.sort_reverse = False
     redraw.redraw_market_table(cli)
 
+def update_buy_options(cli: client):
+    ts10 = commons.get_ts10()
+    print("def update_buy_options")
+    user_account = cli.user_account
+    symbol = cli.symbol
+    print('cli.symbol',symbol)
+    quote,base = commons.split_quote_base(symbol)
+    base_asset = user_account.get(base,0)
+    print('base_asset',base_asset,user_account,base)
+    quote_price = commons.get_quote_price(quote, ts10)
+    print('quote_price',quote_price)
+    base_price = commons.get_base_price(base, ts10)
+    #基准货币可用资产价值
+    base_asset_value = base_asset * base_price
+    #最大能买入的数量
+    quote_can_buy = base_asset_value / quote_price
+    print('quote_can_buy',quote_can_buy)
+    #改变买入数量占总资产百分比，重绘买入数量
+    if cli.buy_amount_perc != pin.buy_amount_perc \
+        or cli.buy_base_amount != pin.buy_base_amount \
+        or cli.buy_quote_amount != pin.buy_quote_amount:
+        #分情况讨论
+        #1. 百分比修改，无论数量是否修改，都按照百分比计算
+        if cli.buy_amount_perc != pin.buy_amount_perc:
+            #空置为0
+            if not pin.buy_amount_perc:
+                pin.buy_amount_perc = 0
+            #调整到0~100之间
+            pin.buy_amount_perc = max(0, min(100, pin.buy_amount_perc))
+            cli.buy_amount_perc = pin.buy_amount_perc
+            #按照百分比计算base_amount数量
+            cli.buy_base_amount = base_asset * pin.buy_amount_perc / 100
+            buy_value = cli.buy_base_amount * base_price
+            #计算quote_amount数量
+            cli.buy_quote_amount = buy_value / quote_price
+            #调整到正确的范围
+            cli.buy_base_amount = max(0, min(base_asset, cli.buy_base_amount))
+            cli.buy_quote_amount = max(0, min(quote_can_buy, cli.buy_quote_amount))
+        #2. 百分比未修改，quote_amount数量修改，按照quote_amount数量计算百分比和base_amount数量
+        elif cli.buy_quote_amount != pin.buy_quote_amount:
+            #空置为0
+            if not pin.buy_quote_amount:
+                pin.buy_quote_amount = 0
+            #调整到0~最大能买入的数量之间
+            pin.buy_quote_amount = max(0, min(quote_can_buy, pin.buy_quote_amount))
+            cli.buy_quote_amount = pin.buy_quote_amount
+            #计算买入总价值
+            buy_value = cli.buy_quote_amount * quote_price
+            #计算买入的base_amount数量
+            cli.buy_base_amount = buy_value / base_price
+            #计算买入的百分比
+            cli.buy_amount_perc = cli.buy_base_amount / base_asset * 100
+            #调整到正确范围
+            cli.buy_base_amount = max(0, min(base_asset, cli.buy_base_amount))
+            cli.buy_amount_perc = max(0, min(100, pin.buy_amount_perc))
+        #3. 百分比未修改，quote_amount数量也未修改，base_amount数量修改，按照base_amount数量计算百分比和quote_amount数量
+        elif cli.buy_base_amount != pin.buy_base_amount:
+            #空置为0
+            if not pin.buy_base_amount:
+                pin.buy_base_amount = 0
+            #调整到0~最大能买入的数量之间
+            pin.buy_base_amount = max(0, min(base_asset, pin.buy_base_amount))
+            cli.buy_base_amount = pin.buy_base_amount
+            #计算买入的百分比
+            cli.buy_amount_perc = cli.buy_base_amount / base_asset * 100
+            #计算买入总价值
+            buy_value = cli.buy_base_amount * base_price
+            #计算买入的quote_amount数量
+            cli.buy_quote_amount = buy_value / quote_price
+            #调整到正确范围
+            cli.buy_quote_amount = max(0, min(quote_can_buy, cli.buy_quote_amount))
+            cli.buy_amount_perc = max(0, min(100, pin.buy_amount_perc))
+        #4. 更新界面pin值
+        pin.buy_amount_perc = cli.buy_amount_perc
+        pin.buy_base_amount = cli.buy_base_amount
+        pin.buy_quote_amount = cli.buy_quote_amount
+
 def trade_price_change(x,cli):
     #价格输入框内容改变事件
     #获取输入框内容
