913b73ef41ffa89459a5755fb8cbb8deef1cc5c4
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-02-07 21:21:11 +0800

    Update redraw.py

--- redraw.py
+++ redraw.py
@@ -202,7 +202,7 @@ def redraw_login_welcome(cli: client):
     put_markdown('### 杠杆率：' + str(commons.get_leverage(user_account)))
     pie_chart_html = commons.get_pie_chart_html(user_account)
     print(pie_chart_html)
-    put_html(pie_chart_html)
+    put_html(pie_chart_html.data)
     users = db.users().read()
     user = users[cli.user_key]
     print("user", user)
