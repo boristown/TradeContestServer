8e4cb68e1b62d2cfc7b48bc548cc9f35c8ac129b
AK自动机 <47687653+boristown@users.noreply.github.com>
2023-02-11 14:53:55 +0800

    Update bg_trade.py

--- bg_trade.py
+++ bg_trade.py
@@ -5,6 +5,7 @@ import json
 connections = set()
 connections.add('wss://stream.binance.com:9443/stream?streams=btcusdt@ticker')
 connections.add('wss://stream.binance.com:9443/stream?streams=ethusdt@ticker')
+connections.add('wss://stream.binance.com:9443/stream?streams=fisusdt@ticker')
 
 async def handle_socket(uri, ):
     counter = 0
@@ -14,13 +15,13 @@ async def handle_socket(uri, ):
             message = json.loads(message)            
             data = message["data"]
             
-            print(f"\n---Raw Data---\n{data}")
-            print("\n---Parsed Data---\n")
-            print(f"STREAM NAME: {message['stream']}")
-            print(f"TIMESTAMP:{data['E']}")
-            print(f"LAST PRICE:{data['c']}")
-            print(f"LAST QUANTITY:{data['q']}")
-            print(f"24h HIGH: {data['h']}")
+            #print(f"\n---Raw Data---\n{data}")
+            #print("\n---Parsed Data---\n")
+            print(f"流名称: {message['stream']}")
+            print(f"时间戳:{data['E']}")
+            print(f"最后价格:{data['c']}")
+            print(f"最后交易量:{data['q']}")
+            print(f"24小时最高价: {data['h']}")
             print("\n------\n")
 async def handler():
     await asyncio.gather(*[handle_socket(uri) for uri in connections])
